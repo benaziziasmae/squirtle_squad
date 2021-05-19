96f858c4cbaa9264bb169f0a4929f57c11487f8f
Ian Fan <ian.fan13@gmail.com>
2021-05-19 18:54:45 -0400

    OCR tesseract- sort of working

--- Machine_Learning/object_detection_image.py
+++ Machine_Learning/object_detection_image.py
@@ -3,4 +3,9 @@ import numpy as np
 import tensorflow as tf
 import sys
 import cv2
+import pytesseract
 
+file_dir = os.path.abspath('')+"\\Resources"
+file_path = os.path.join(file_dir, "cramorant.png")
+print(file_path)
+image_file = cv2.imread()
