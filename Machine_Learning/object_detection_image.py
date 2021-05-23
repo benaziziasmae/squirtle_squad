569aa5d6156b8962bf6c12795bdfa68cf3e6397c
Ian Fan <ian.fan13@gmail.com>
2021-05-23 15:52:01 -0400

    rename training images, tesseract & open CV

--- Machine_Learning/object_detection_image.py
+++ Machine_Learning/object_detection_image.py
@@ -1,11 +1,10 @@
 import os
-import numpy as np
-import tensorflow as tf
-import sys
 import cv2
-import pytesseract
 
-file_dir = os.path.abspath('')+"\\Resources"
+file_dir = os.path.abspath('')+"\\train"
 file_path = os.path.join(file_dir, "cramorant.png")
-print(file_path)
-image_file = cv2.imread()
+image_file = cv2.imread(file_path)
+
+cv2.imshow('image', image_file)
+cv2.waitKey()
+cv2.destroyAllWindows()
