c2dea0664bf258d46aec2d9c9357a1a23db50cf0
unknown <you@example.com>
2021-05-30 19:55:52 -0400

    adding home page asmae

--- squirtle_squad visualization/IF/app.py
+++ squirtle_squad visualization/IF/app.py
@@ -1,15 +1,41 @@
 from flask import Flask, jsonify, render_template
 import json
-#create a simple falsk to return the HTML
+import sys
+sys.path.append(".")
+from config import tcgapikey, pgpassword
+from sqlalchemy import create_engine
+
+
+
+
+# create a simple falsk to return the HTML
 app = Flask(__name__)
 
-@app.route("/home")
+# making the connection
+engine = create_engine('postgresql://postgres:{pgpassword}@localhost:5432/PokemonTCG', echo=False)
+sample_collector_number = '062/202'
+pokemon_Data = None
+
+@app.route("/")
 def welcome(): 
+    
+    # on submission of image (WIP)
+
+    # run machine learning to identify query (result = output variable) (WIP)
+
+    
+    ## Do stuff with database connection (run query)
+    
+    # query engine with result (use sample_collector_number for now)
+
+    # assign global? varaible or pass directly to new html
     
     return render_template("home.html")
 
 @app.route("/visualization")
 def returnedCard():
+
+    
     pokemon_Data = {
         "id":"swsh35-2",
         "name":"Weedle",
@@ -28,7 +54,6 @@ def returnedCard():
         "set_name":"Champion's Path",
         "date":1622239842548}
 
+    
+
     return render_template("visualization.html", card_data=pokemon_Data)
-
-
-
